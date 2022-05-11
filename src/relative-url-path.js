4cceb0488f8cdffb3349b220f3eb39dd568d38bf
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2022-05-10 23:11:18 -0400

    Initial Creation

---
+++ src/relative-url-path.js
@@ -0,0 +1,16 @@
+class RelativeUrl
+{
+    constructor()
+	{
+		this.origin = location.origin;
+		this.pathName = location.pathName;
+
+		CreateRelativePath(this.pathName);
+	}
+
+	CreateRelativePath(path)
+	{
+		const path;
+	}
+
+}
